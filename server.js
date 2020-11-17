f570e33dee7e60753429b8b094c85700e2fe75a7
charan <charangajjala7@gmail.com>
2020-11-17 15:06:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app=express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>
+    res.send('API is running')
+);
+
+app.listen(PORT,()=>console.log(`Server started at port ${PORT}`));
