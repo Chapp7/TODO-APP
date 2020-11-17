ebfd4f9d9c983282940dbd9682418a5586f66df4
charan <charangajjala7@gmail.com>
2020-11-17 15:19:48 +0530

    Database connected

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app=express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get('/', (req, res)=>
     res.send('API is running')
