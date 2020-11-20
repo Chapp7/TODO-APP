fa6cc8cd40b7fc15a5af80721fc9f4718966bbcf
Prem01-cyber <74588553+Prem01-cyber@users.noreply.github.com>
2020-11-20 20:04:33 +0530

    clieny=t side code

---
+++ Todo/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard';
+import Navbar from './components/layout/Navbar'
+import TaskDetails from './components/tasks/TaskDetails'
+import SignIn from './components/authentication/SignIn'
+import SignUp from './components/authentication/SignUp'
+import CreateTask from './components/tasks/CreateTask'
+
+class App extends Component {
+  render(){
+    return(
+      <BrowserRouter>
+        <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path= '/' component={Dashboard}/>
+          <Route path= '/project/:id' component={TaskDetails}/>
+          <Route path= '/signin' component={SignIn}/>
+          <Route path='/signup' component={SignUp}/>
+          <Route path='/create' component={CreateTask}/>
+        </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
---
+++ Todo/src/store/reducers/taskReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  tasks:[
+    {id: '1', title: 'this is task 1',content:'it should becompleted by tommorrow'}
+  ]
+}
+  
+
+const taskReducer =(state = initialState,action)=>{
+  return state
+}
+export default taskReducer
